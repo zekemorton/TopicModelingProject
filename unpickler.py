a06d6a2eb17e3e7167e99b326e334c4a44e7b46b
Katya Katsy <kkatsy@ucdavis.edu>
2020-11-24 17:52:07 -0800

    added clean socbio articles directory

--- unpickler.py
+++ unpickler.py
@@ -2,7 +2,7 @@ import pickle
 
 # see contents of pickled files
 
-FILENAME = "equarterly_1961_8-2_81.pickle"
+FILENAME = "socbio_clean_articles/socbio_1978_25-3_Social Biology, 25:3, 258.pickle"
 pickle_off = open(FILENAME, "rb")
 contents = pickle.load(pickle_off)
 
