eed28d042b0c70c36acd483927e5263dea9bada3
Zeke Morton <morton30@llnl.gov>
2020-12-13 19:05:56 -0800

    Added 11 topic model

--- unpickler.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-# see contents of pickled files
-
-FILENAME = "equarterly_1961_8-2_81.pickle"
-pickle_off = open(FILENAME, "rb")
-contents = pickle.load(pickle_off)
-
-print(contents)
