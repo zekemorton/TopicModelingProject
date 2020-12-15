a03040d08f5075eaef2b1b86bfe1cb85e72f8632
Katya Katsy <kkatsy@ucdavis.edu>
2020-12-14 21:32:01 -0800

    restructured files

--- unpickler.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-# see contents of pickled files
-
-FILENAME = "/Users/kk/PycharmProjects/TopicModelingProject/ereview_clean_articles/ereview_><h2_2-1_00396-0033.pickle"
-pickle_off = open(FILENAME, "rb")
-contents = pickle.load(pickle_off)
-
-print(contents)
